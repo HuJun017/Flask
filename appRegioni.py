2cf86052d492e7dd43873ca7ecde42c01b22e58c
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-04-22 08:58:24 +0000

    added interactive regions

--- appRegioni.py
+++ appRegioni.py
@@ -10,5 +10,19 @@ def about():
     nomi.sort()
     return render_template('regioni.html', regioni=nomi)
 
+@app.route('/info/<regione>', methods=['GET'])
+def info(regione): 
+    import pandas as pd
+    regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv' )
+    info = regioni_italiane[regioni_italiane.denominazione_regione == regione.capitalize()]
+    return render_template('table.html', tabella = info.to_html())
+
+@app.route('/info_json/<regione>', methods=['GET'])
+def info_json(regione): 
+    import pandas as pd
+    regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv' )
+    info = regioni_italiane[regioni_italiane.denominazione_regione == regione.capitalize()]
+    return render_template('json.html', tabella = info.to_json())
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
