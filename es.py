429a55cd45c1813bf1096e6c4b697a4d86b605a8
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-04-24 13:37:41 +0000

    updated get region code and html

--- es.py
+++ es.py
@@ -1,18 +1,21 @@
 '''
-Realizzare un single page application che permette di avere le informazioni sulle regioni italiane.
-In un file CSV devono essere memeorizzare le informazioni:
-- il nome della regione
-- il nome del capoluogo
-- percorso di un'immagine relativa alla regione.
-L'applicazione deve pemettre all'utente di selezionare da un elenco il nome della regione e deve fornire all'utente tutte le informazioni realtive 
-alla regione nonchè il meteo realtivo al capoluogo di quella regione. Grafica a scelta
+realizzare un single page application che permette di avere informazioni sulle regioni italiane.
+in un file csv devono essere memorizzate le seguenti informazioni: 
+-nome della regione
+-nome del capoluogo
+-popolazione della regione
+-popolazione del capoluogo
+-percorso di un' immagine relativa alla regione.
+l'applicazione deve permettere all'utente di selezionare da un elenco il nome della regione e 
+deve fornire all'utente tutte le informazioni relative alla regione nonché il meteo relativo al
+capoluogo di quella regione. Grafica a scelta.
 '''
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    #questa rotta resituisce la la homepage della single page application
+    #questa rotta restituisce la homepage della nostra single page application
     import pandas as pd
     df = pd.read_csv('/workspace/Flask/data/regioni.csv')
     risultato = sorted(list(set(df['nome_regione'])))
@@ -22,15 +25,15 @@ def homepage():
 def elenco():
     import pandas as pd
     df = pd.read_csv('/workspace/Flask/data/regioni.csv')
-    info = df.nomeRegione
-    return render_template('json.html', lista = info.to_json())
+    info = df.nome_regione
+    return render_template('json.html', tabella = info.to_json())
 
 @app.route('/info/<nomeRegione>')
 def info(nomeRegione):
     import pandas as pd
     df = pd.read_csv('/workspace/Flask/data/regioni.csv')
     info = df[df.nome_regione == nomeRegione]
-    return render_template('json.html', lista = info.to_json())
+    return render_template('json.html', tabella = info.to_json())
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3246, debug=True)
--- static/js/es.js
+++ static/js/es.js
@@ -1,11 +1,10 @@
-function get_elenco() {
-    fetch('https://3245-hujun017-flask-i1ii7bh5k9w.ws-eu110.gitpod.io/elenco')
+function getElenco() {
+    fetch('https://3246-hujun017-flask-i1ii7bh5k9w.ws-eu110.gitpod.io/elenco')
     .then(response => response.json())
     .then(data => {
-        document.getElementById('elenco').innerHTML = data
         console.log(data)
         for (let regione in data) {
-            document.getElementById('elenco').innerHTML += data[regione] + "<br />"
+            document.getElementById('elenco').innerHTML += data[regione] + '<br />'
         }
     })
 }
